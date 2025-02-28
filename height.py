7bb084a0a3249924dc8b2bbbab7bf0b18d194d90
Mohith-Reddy-07 <manchurumohith07@gmail.com>
2025-02-27 20:11:27 -0600

    prog4

---
+++ height.py
@@ -0,0 +1,12 @@
+def peope_height(names: list[str], heights: list[int]) -> list[str]:
+    
+    people = list(zip(names,heights))
+    
+    people.sort(key=lambda x: x[1], reverse=True)
+    
+    return [person[0] for person in people]
+
+names = input("enter the names of people: ").split(" ")
+heights = list(map(int,input("enter the heights of the people: ").split(" ")))
+
+print(peope_height(names, heights))
